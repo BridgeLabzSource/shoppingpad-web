f24ef2f89195ebc3e1ed667d01efd131bbce6560
Anurag-Sing <mail.anurag0707@gmail.com>
2016-04-27 15:09:19 +0530

    second

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-angular.module('starterApp', ['ngMaterial']);
+angular.module('starterApp', ['ngMaterial'])
+
+.controller('mainController', function($scope) {
+
+    // function to submit the form after all validation has occurred
+    $scope.submitForm = function() {
+
+        // check to make sure the form is completely valid
+
+    };
+
+});
