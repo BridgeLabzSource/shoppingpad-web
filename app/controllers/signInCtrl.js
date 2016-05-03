ba97bac80e8e853b9548e4c288071b1b47c66d21
Anurag-Sing <mail.anurag0707@gmail.com>
2016-05-03 10:21:15 +0530

    2nd commit

---
+++ app/controllers/signInCtrl.js
@@ -0,0 +1,25 @@
+angular.module('starterApp')
+    .controller('signInCtrl', function($scope,$state) {
+
+
+        //$rootScope.name=$scope.name;
+        //console.log(name)
+
+        var storage;
+
+        $scope.signIn=function(){
+
+            storage=[$scope.name,$scope.security]
+            console.log(storage);
+            localStorage.setItem("passingData",JSON.stringify(storage))
+
+
+           //$localStorage.LocalMessage=$scope.name+"   "+$scope.security;;
+           // console.log($localStorage.LocalMessage)
+            $state.go('login')
+            console.log("inside signin")
+
+
+
+            }
+    });
